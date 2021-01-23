dd6885362eb279b3f8129fb88f30e7200d599d07
PuppyPi <puppyofcodez@gmail.com>
2021-01-23 02:15:08 +0000

    Testing data really shouldn't be in srcs/   ^^'

--- srcs/python-version/tests/test_lc2e_config_file_parser.py
+++ srcs/python-version/tests/test_lc2e_config_file_parser.py
@@ -3,10 +3,9 @@
 import sys, os;
 
 TopPythonDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TopDir = os.path.dirname(TopPythonDir)
+TopDir = os.path.dirname(os.path.dirname(TopPythonDir))
 
 sys.path.append(os.path.join(TopPythonDir, "main"))
-print(sys.path)
 
 from transientlc2e import *;
 
@@ -32,6 +31,9 @@ if (len(args) == 0):
 	
 	if (os.path.isdir(defaultTestDataDir)):
 		args = map(lambda n: os.path.join(defaultTestDataDir, n), os.listdir(defaultTestDataDir));
+	else:
+		print("Couldn't find the test-data dir! D:")
+		sys.exit(8)
 
 for targetFile in args:
 	print("Testing on "+repr(os.path.basename(targetFile))+" ... (cross your hooves!)  :>!");
